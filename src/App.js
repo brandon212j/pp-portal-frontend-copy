f3c8dfee6c0e62cdb1da0e87a46937ae23984261
brandon212j <148082943+brandon212j@users.noreply.github.com>
2024-06-07 12:04:10 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import React, { useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+//
+export default App;
